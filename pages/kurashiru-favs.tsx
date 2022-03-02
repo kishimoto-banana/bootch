62ca576cb43a16519d7c18fd7c34d9b280da70e7
masashi kishimoto <drehbleistift@gmail.com>
2022-03-02 23:37:57 +0900

    更新日時

--- pages/kurashiru-favs.tsx
+++ pages/kurashiru-favs.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps, NextPage } from 'next'
+import { formatToTimeZone } from 'date-fns-timezone'
 
 type Recipe = {
   url: string
@@ -10,24 +11,30 @@ type Recipes = Recipe[]
 
 type Props = {
   recipes: Recipes
+  fetchedAt: string
 }
 
-const KURASHIRU_FAV_API_ENDPOINT =
+const kurashiruFavApiEndpoint =
   'https://script.google.com/macros/s/AKfycbyaQbxvKkLoGeVoCOiaCpbfTOm3QC5Q-hfZ-YwMKQDa7F4OCzlvb0BQh7IoxEru7P1ohQ/exec'
-const REVALIDATE_SECOND = 60 * 30 // 30分
+const revalidateSecond = 10 // 30分
+const formatStyle = 'YYYY-MM-DDD HH:mm:ss'
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  const res = await fetch(KURASHIRU_FAV_API_ENDPOINT, { redirect: 'follow' })
+  const fetchedAt = formatToTimeZone(new Date(), formatStyle, {
+    timeZone: 'Asia/Tokyo',
+  })
+  const res = await fetch(kurashiruFavApiEndpoint, { redirect: 'follow' })
   const recipes: Recipes = await res.json()
-  return { props: { recipes }, revalidate: REVALIDATE_SECOND }
+  return { props: { recipes, fetchedAt }, revalidate: revalidateSecond }
 }
 
-const KurashiruFavs: NextPage<Props> = ({ recipes }) => {
+const KurashiruFavs: NextPage<Props> = ({ recipes, fetchedAt }) => {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center">
       <h3 className="pt-8 text-2xl font-medium">
         ぼくのクラシルお気に入りレシピ
       </h3>
+      <p className="pt-2 text-sm">最終更新日時：{fetchedAt}</p>
       <div className="my-12 grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {recipes.map((recipe) => (
           <div className="max-w-recipeCard">
